8c069141f277585f70802945f6a3c16c786611e2
Пользователь <tazidinovusman@gmail.com>
2026-04-14 09:36:47 +0600

    fix router version

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
 import './index.css';
 
